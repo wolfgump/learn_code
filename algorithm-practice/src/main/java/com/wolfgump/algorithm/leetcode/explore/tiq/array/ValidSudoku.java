6762bc3eff2acc034e3f295738ec217e93ed4a9d
shitao.gan <shitao.gan@zhangmen.com>
2021-07-16 10:13:36 +0800

    validate sudoku

---
+++ algorithm-practice/src/main/java/com/wolfgump/algorithm/leetcode/explore/tiq/array/ValidSudoku.java
@@ -0,0 +1,104 @@
+package com.wolfgump.algorithm.leetcode.explore.tiq.array;
+
+import java.util.HashSet;
+
+/**
+ * @author ShiTao.Gan
+ * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ * <p>
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * Note:
+ * <p>
+ * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * Only the filled cells need to be validated according to the mentioned rules.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: board =
+ * [["5","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: board =
+ * [["8","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * board.length == 9
+ * board[i].length == 9
+ * board[i][j] is a digit or '.'.
+ **/
+public class ValidSudoku {
+    public static boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>(81);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                char data = board[i][j];
+                if(data=='.'){
+                    continue;
+                }
+                if (!set.add(data + "row" + i)) {
+                    return false;
+                }
+                if (!set.add(data + "column" + j)) {
+                    return false;
+                }
+                if (!set.add(data + "column" + j / 3 + "row" + i / 3)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] board1 = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        char[][] board2 = {
+                {'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        boolean result1 = isValidSudoku(board1);
+        boolean result2 = isValidSudoku(board2);
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+}
